cddbb7309d09f6a0d39c1248d658556017ed4ad2
acer <acer@LAPTOP-ESFJ9H96>
2022-06-14 01:01:04 +0700

    Add project files.

---
+++ BigSchool_HuynhTanHuy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_HuynhTanHuy.Startup))]
+namespace BigSchool_HuynhTanHuy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ BigSchool_HuynhTanHuy/ViewModels/CustomViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool_HuynhTanHuy.ViewModels
+{
+    public class CustomViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Date { get; set; }
+        [Required]
+        public string Time { get; set; }   
+    }
+}
